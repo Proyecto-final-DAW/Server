b285fd2fff99943c6b02f3160d53ffefa6a9a3ae
nblancoz <nblancozappala@gmail.com>
2026-04-25 17:40:57 +0200

    feat: implement bruteforce protection middleware for login and registration

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,13 +3,21 @@ import express from 'express';
 import MacrosController from '../controllers/MacrosController';
 import UserController from '../controllers/UserController';
 import { authentication } from '../middlewares/auth';
+import {
+  loginBruteforceProtection,
+  registerBruteforceProtection,
+} from '../middlewares/bruteforce';
 import { ensureSelf } from '../middlewares/ensureSelf';
 
 const router = express.Router();
 
 // Auth
-router.post('/auth/register', UserController.register);
-router.post('/auth/login', UserController.login);
+router.post(
+  '/auth/register',
+  registerBruteforceProtection,
+  UserController.register
+);
+router.post('/auth/login', loginBruteforceProtection, UserController.login);
 router.post('/auth/logout', authentication, UserController.logout);
 
 router.post(
